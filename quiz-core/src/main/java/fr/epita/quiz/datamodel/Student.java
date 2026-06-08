12958609efccc915b2c70003a976721969b022ce
thomasbroussard <thomas.broussard@gmail.com>
2026-06-08 10:16:01 +0200

    spring integration: quiz-core

---
+++ quiz-core/src/main/java/fr/epita/quiz/datamodel/Student.java
@@ -0,0 +1,7 @@
+package fr.epita.quiz.datamodel;
+
+public class Student {
+    private String name;
+    private String address;
+
+}
---
+++ quiz-core/src/test/java/fr/epita/quiz/test/TestDAO.java
@@ -0,0 +1,15 @@
+package fr.epita.quiz.test;
+
+import org.junit.jupiter.api.Test;
+
+public class TestDAO {
+
+    @Autowired
+
+
+    @Test
+    public void test(){
+
+
+    }
+}
